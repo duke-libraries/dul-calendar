25752766d9e3e7f901d1b140619173159e6e2ff0
Derrek Croney <derrek@decro.net>
2015-02-23 12:25:58 -0500

    added js file that reads settings created in drupal page callback

---
+++ js/decro_tutorial.init.js
@@ -0,0 +1,23 @@
+// Javascript for decro Tutorial module
+(function($) {
+	Drupal.behaviors.decro_tutorial = {
+		attach : function(context, settings) {
+			
+			// the settings that were created in the 
+			// page callback are accessible in this manner
+			s = settings.decro_tutorial;
+			
+			// now, I'm going to add markup to the #decro-container DOM element
+			// that was also created in the page callback
+			
+			debugger;
+			$('#decro-container').html(
+				'<div>' + 
+				'The specified team ID was: ' + s.team_id + '<br />' +
+				'The specified sport ID was: ' + s.team_sport + '<br />' +
+				'Finally, the specified season was ' + s.season +
+				'</div>'
+			);
+		}
+	}	
+})(jQuery);
