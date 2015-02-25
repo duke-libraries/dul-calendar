477ee7536f0beeaa281e4a34960ff57de80e1d65
Derrek Croney <derrekc@users.noreply.github.com>
2015-02-25 09:58:46 -0500

    Update decro_tutorial.init.js

--- js/decro_tutorial.init.js
+++ js/decro_tutorial.init.js
@@ -1,22 +1,12 @@
-// Javascript for decro Tutorial module
+// Javascript for dul_calendar module
 (function($) {
-	Drupal.behaviors.decro_tutorial = {
+	Drupal.behaviors.dul_calendar = {
 		attach : function(context, settings) {
 			
 			// the settings that were created in the 
 			// page callback are accessible in this manner
-			s = settings.decro_tutorial;
+			s = settings.dul_calendar;
 			
-			// now, I'm going to add markup to the #decro-container DOM element
-			// that was also created in the page callback
-			
-			$('#decro-container').html(
-				'<div>' + 
-				'The specified team ID was: ' + s.team_id + '<br />' +
-				'The specified sport ID was: ' + s.team_sport + '<br />' +
-				'Finally, the specified season was: ' + s.season +
-				'</div>'
-			);
 		}
 	}	
 })(jQuery);
