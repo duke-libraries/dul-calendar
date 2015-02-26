03db74d6a7b14d4f935726ddd2243de0c7d1fe85
Derrek Croney <dlc32@duke.edu>
2015-02-26 10:31:00 -0500

    created initial API definition

---
+++ dul_calendar.api.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Describe hooks provided by the DUL Calendar  module.
+ */
+
+function hook_dul_calendar_hours_elements($dataArray, $context = NULL) {
+}
+
+function hook_dul_calendar_events_elements($dateArray, $context = NULL) {
+}
+
+function hook_dul_calendar_elements($dateArray, $context = NULL) {
+}
+
